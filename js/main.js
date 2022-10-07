b808c7e183f7cfa9c3617e2e5cba10fc58176e2b
Jordan Wolter <jwolter456@gmail.com>
2022-10-07 13:37:48 -0400

    added colors buttons transition and gradient

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 // Write your DOM code here!
+
+
+let red = document.querySelector(`.red`)
+
+red.addEventListener(`click`, () => {
+    document.body.classList.toggle(`red`)
+})
+
+let white = document.querySelector(`.white`)
+
+white.addEventListener(`click`, () => {
+    document.body.classList.toggle(`white`)
+})
+
+let blue = document.querySelector(`.blue`)
+
+blue.addEventListener(`click`, () => {
+    document.body.classList.toggle(`blue`)
+})
+
+let yellow = document.querySelector(`.yellow`)
+
+yellow.addEventListener(`click`, () => {
+    document.body.classList.toggle(`yellow`)
+})
+
+let grad = document.querySelector(`.gradient`)
+
+grad.addEventListener(`click`, () => {
+    document.body.classList.toggle(`gradient`)
+})
+
